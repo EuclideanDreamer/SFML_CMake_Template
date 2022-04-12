758b0f9614ed2cfbfa8979f4d8e06787e8160b9b
EuclideanDreamer <78236576+EuclideanDreamer@users.noreply.github.com>
2022-04-12 06:50:19 -0700

    Add files via upload

---
+++ SFML_Project_Template/Headers/header.h
@@ -0,0 +1,21 @@
+/*
+	Author:
+	
+	Purpose:
+
+	Structure:
+
+	Notes:
+
+*/
+#define HEADER_H
+#ifdef  HEADER_H
+
+#include <SFML/Graphics.hpp>
+//
+//there is stuff in this file github!
+
+void vF(){}
+
+
+#endif
